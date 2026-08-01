2f5fa9c0eb417aca805dbbe31870452a3cb1e8a2
duhfreakinduh <duhfreakinduh@gmail.com>
2026-08-01 00:07:48 -0500

    Refresh cached scanner files

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'qr-anything-v4';
-const ASSETS = ['./', './index.html', './styles.css', './app-part1.js', './app-part2.js', './fixes.js', './transfer-fixes.js', './manifest.webmanifest', './icon.svg'];
+const CACHE = 'qr-anything-v5';
+const ASSETS = ['./', './index.html', './styles.css', './app-part1.js', './app-part2.js', './fixes.js', './transfer-fixes.js', './scan-fix-v5.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
